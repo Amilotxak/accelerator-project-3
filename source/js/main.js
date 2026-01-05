6eda07c42a2d7d7ec0df748283211b699ff54c8e
Dmitry <56439852+Amilotxak@users.noreply.github.com>
2026-01-06 00:01:50 +0300

    добавляет начало стилизации разметки и неудачную попытку прикрутить swiper :(

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,39 @@
 // https://swiperjs.com/get-started#installation
-// import Swiper from "swiper";
-// import {Navigation, Pagination} from "swiper/modules";
-// import 'swiper/css';
+
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
+import 'swiper/css';
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const swiper = new Swiper(".swiper", {
+        loop: true, // Зацикливание слайдов
+        autoplay: {
+            delay: 5000, // Автопрокрутка каждые 5 секунд
+            disableOnInteraction: false, // Автопрокрутка не останавливается при взаимодействии
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        effect: "fade", // Плавное появление нового слайда
+        fadeEffect: {
+            crossFade: true,
+        },
+        on: {
+            init: function () {
+                // Показываем контент на первом активном слайде
+                document
+                    .querySelectorAll(".swiper-slide-active .slide-content")
+                    .forEach((el) => {
+                        el.style.opacity = "1";
+                        el.style.transform = "translateY(0)";
+                    });
+            },
+        },
+    });
+});
