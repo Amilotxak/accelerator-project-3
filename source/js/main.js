1a5c531b89b687b19f5f60c7b70c41c757d70c61
Dmitry <56439852+Amilotxak@users.noreply.github.com>
2026-01-11 01:59:22 +0300

    правки и стилизация

--- source/js/main.js
+++ source/js/main.js
@@ -4,8 +4,8 @@ import Swiper from "swiper";
 import { Navigation, Pagination } from "swiper/modules";
 import 'swiper/css';
 
-let navMain = document.querySelector('.header-nav');
-let navToggle = document.querySelector('.header-nav__toggle');
+let navMain = document.querySelector('.header__nav');
+let navToggle = document.querySelector('.header__nav-toggle');
 
 document.addEventListener("DOMContentLoaded", function () {
     const swiper = new Swiper(".swiper", {
@@ -42,42 +42,42 @@ document.addEventListener("DOMContentLoaded", function () {
 
 // меню навигации
 
-navMain.classList.remove('header-nav--nojs');
+navMain.classList.remove('header__nav--nojs');
 
 navToggle.addEventListener('click', function () {
-    navMain.classList.toggle('header-nav--closed');
-    navMain.classList.toggle('header-nav--opened');
+    navMain.classList.toggle('header__nav--closed');
+    navMain.classList.toggle('header__nav--opened');
 });
 
 // вопросы и ответы 
 
-let questinsItem = document.querySelector('.questins-item');
-let questinsButton = document.querySelector('.questins-item-button');
+let questinsItem = document.querySelector('.faq__item');
+let questinsButton = document.querySelector('.faq__item-toggle');
 
 
-document.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('.questins').addEventListener('click', function(event) {
-    const button = event.target.closest('.questins-item-button');
-    
-    if (button) {
-      const item = button.closest('.questins-item');
-      const willOpen = item.classList.contains('questins-item--closed');
-      
-      // управление классами
-      item.classList.toggle('questins-item--closed');
-      item.classList.toggle('questins-item--opened');
-      
-      // второй класс копирует состояние первого
-      if (willOpen) {
-        item.classList.add('questins-item--open');
-      } else {
-        item.classList.remove('questins-item--open');
-      }
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('.faq').addEventListener('click', function (event) {
+        const button = event.target.closest('.faq__item-toggle');
 
-      const path = button.querySelector('svg path');
-      if (path) {
-        path.setAttribute('d', willOpen ? 'M0 9H18' : 'M9 0V9M9 9V18M9 9H0M9 9H18');
-      }
-    }
-  });
+        if (button) {
+            const item = button.closest('.faq__item');
+            const willOpen = item.classList.contains('faq__item--closed');
+
+            // управление классами
+            item.classList.toggle('faq__item--closed');
+            item.classList.toggle('faq__item--opened');
+
+            // второй класс копирует состояние первого
+            if (willOpen) {
+                item.classList.add('faq__item--open');
+            } else {
+                item.classList.remove('faq__item--open');
+            }
+
+            const path = button.querySelector('svg path');
+            if (path) {
+                path.setAttribute('d', willOpen ? 'M0 9H18' : 'M9 0V9M9 9V18M9 9H0M9 9H18');
+            }
+        }
+    });
 });
